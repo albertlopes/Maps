8e1947448a66054bb59055782d0cd57784886cf1
albertlopes <albert.abt@gmail.com>
2018-10-20 12:28:04 -0300

    fszd

--- app/src/main/java/br/com/albertlopes/maps/MapsActivity.kt
+++ app/src/main/java/br/com/albertlopes/maps/MapsActivity.kt
@@ -1,9 +1,15 @@
 package br.com.albertlopes.maps
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Color
-import android.location.Geocoder
+import android.location.*
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
+import br.com.albertlopes.maps.utils.PermissaoUtils
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -15,17 +21,80 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import java.util.*
 
+
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    val permissoesLocalizacao = listOf(Manifest.permission.ACCESS_FINE_LOCATION)
+
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+
+        PermissaoUtils.validaPermissao(permissoesLocalizacao.toTypedArray(), this, 1)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        for (resposta in grantResults) {
+            if (resposta == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(applicationContext, "Sem permissao sem acesso", Toast.LENGTH_LONG).show()
+            } else {
+
+                onRequestLocationUpdate()
+
+            }
+        }
+    }
+
+
+    private fun onRequestLocationUpdate() {
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            var locationManager = getSystemService(Context.LOCATION_SERVICE)
+                    as LocationManager
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+
+        }
+    }
+
+    private fun initLocationListener() {
+
+        locationListener = object : LocationListener {
+
+            override fun onLocationChanged(location: Location?) {
+
+                val minhaPosicao = LatLng(location?.latitude!!, location?.longitude!!)
+                addMarcador(minhaPosicao, "posicao no maps")
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(minhaPosicao, 12f))
+
+            }
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+            }
+
+            override fun onProviderEnabled(provider: String?) {
+
+            }
+
+            override fun onProviderDisabled(provider: String?) {
+
+            }
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -34,20 +103,21 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mMap.setOnMapClickListener {
 
-            addMarcador(it, getAdress(it))
+          //  addMarcador(it, getAdress(it))
 
         }
 
         mMap.setOnMapLongClickListener {
 
 
-            addMarcador(it, getAdress(it))
+          //  addMarcador(it, getAdress(it))
 
         }
 
-        addFiaps()
+       // addFiaps()
 
-
+        initLocationListener()
+        onRequestLocationUpdate()
 
     }
 
@@ -89,12 +159,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 .title(getAdress(fiapVilaOlimpia)))
 
 
-        val circulo =  CircleOptions()
+        val circulo = CircleOptions()
 
         circulo.radius(200.0)
         circulo.visible(true)
-        circulo.fillColor(Color.argb(50,255,0,255))
-        circulo.strokeColor(Color.argb(255,255,0,255))
+        circulo.fillColor(Color.argb(50, 255, 0, 255))
+        circulo.strokeColor(Color.argb(255, 255, 0, 255))
         circulo.center(fiapPaulista)
 
         mMap.addCircle(circulo)
